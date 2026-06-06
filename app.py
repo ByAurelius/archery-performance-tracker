6ac026af327c40fbce617d7235d029fa1ad994a6
arda akgul <ardaakgl07@gmail.com>
2026-06-06 18:22:45 +0200

    Added proof photo upload to interactive scorecard and enabled coach viewing

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -13,17 +14,21 @@ app.config['UPLOAD_FOLDER'] = 'static/uploads'
 
 db = SQLAlchemy(app)
 
-
 class Archer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     is_coach = db.Column(db.Boolean, default=False)
+    first_name = db.Column(db.String(50), nullable=True)
+    last_name = db.Column(db.String(50), nullable=True)
+    age = db.Column(db.Integer, nullable=True)
+    weight = db.Column(db.Float, nullable=True)
+    height = db.Column(db.Float, nullable=True)
+    draw_length = db.Column(db.Float, nullable=True)
     scores = db.relationship('Score', backref='shooter', lazy=True)
     sight_marks = db.relationship('SightMark', backref='shooter', lazy=True)
     training_plans = db.relationship('TrainingPlan', backref='receiver', lazy=True)
-
 
 class Score(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -32,18 +37,17 @@ class Score(db.Model):
     arrows_shot = db.Column(db.Integer, nullable=False)
     total_score = db.Column(db.Integer, nullable=False)
     image_file = db.Column(db.String(255), nullable=True)
+    arrow_data = db.Column(db.Text, nullable=True)
+    arrows_per_end = db.Column(db.Integer, default=6)
     archer_id = db.Column(db.Integer, db.ForeignKey('archer.id'), nullable=False)
-
 
 class SightMark(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date_recorded = db.Column(db.String(20), nullable=False)
     distance = db.Column(db.Integer, nullable=False)
     setting = db.Column(db.String(50), nullable=False)
-    # YENI SUTUN: Nisangah fotografi icin dosya adi yeri (Zorunlu degil)
     image_file = db.Column(db.String(255), nullable=True)
     archer_id = db.Column(db.Integer, db.ForeignKey('archer.id'), nullable=False)
-
 
 class TrainingPlan(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -53,92 +57,68 @@ class TrainingPlan(db.Model):
     is_completed = db.Column(db.Boolean, default=False)
     athlete_id = db.Column(db.Integer, db.ForeignKey('archer.id'), nullable=False)
 
-
 with app.app_context():
     db.create_all()
 
-
 @app.route('/')
-def home_page():
-    return render_template('index.html')
-
+def home_page(): return render_template('index.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register_page():
     if request.method == 'POST':
-        form_username = request.form.get('username')
-        form_email = request.form.get('email')
-        form_password = request.form.get('password')
-        form_is_coach = request.form.get('is_coach') == 'on'
-
-        hashed_password = generate_password_hash(form_password)
-        new_archer = Archer(username=form_username, email=form_email, password=hashed_password, is_coach=form_is_coach)
+        hashed_password = generate_password_hash(request.form.get('password'))
+        new_archer = Archer(username=request.form.get('username'), email=request.form.get('email'),
+                            password=hashed_password, is_coach=(request.form.get('is_coach') == 'on'))
         db.session.add(new_archer)
         db.session.commit()
         return redirect(url_for('login_page'))
     return render_template('register.html')
 
-
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
     if request.method == 'POST':
-        form_email = request.form.get('email')
-        form_password = request.form.get('password')
-        archer = Archer.query.filter_by(email=form_email).first()
-
-        if archer and check_password_hash(archer.password, form_password):
+        archer = Archer.query.filter_by(email=request.form.get('email')).first()
+        if archer and check_password_hash(archer.password, request.form.get('password')):
             session['user_id'] = archer.id
             session['username'] = archer.username
             session['is_coach'] = archer.is_coach
-            if archer.is_coach:
-                return redirect(url_for('coach_dashboard_page'))
-            else:
-                return redirect(url_for('dashboard_page'))
-        else:
-            return "<h1>Invalid Email or Password!</h1>"
+            return redirect(url_for('coach_dashboard_page') if archer.is_coach else url_for('dashboard_page'))
+        return "<h1>Invalid Email or Password!</h1>"
     return render_template('login.html')
 
+@app.route('/profile', methods=['GET', 'POST'])
+def profile_page():
+    if 'user_id' not in session: return redirect(url_for('login_page'))
+    archer = Archer.query.get(session['user_id'])
+    if request.method == 'POST':
+        archer.first_name, archer.last_name = request.form.get('first_name'), request.form.get('last_name')
+        archer.age, archer.weight = request.form.get('age'), request.form.get('weight')
+        archer.height, archer.draw_length = request.form.get('height'), request.form.get('draw_length')
+        db.session.commit()
+        return redirect(url_for('profile_page'))
+    return render_template('profile.html', archer=archer)
 
 @app.route('/coach_dashboard')
 def coach_dashboard_page():
     if 'user_id' in session and session.get('is_coach'):
-        athletes = Archer.query.filter_by(is_coach=False).all()
-        return render_template('coach_dashboard.html', username=session['username'], athletes=athletes)
-    else:
-        return redirect(url_for('home_page'))
-
+        return render_template('coach_dashboard.html', username=session['username'], athletes=Archer.query.filter_by(is_coach=False).all())
+    return redirect(url_for('home_page'))
 
 @app.route('/athlete/<int:athlete_id>')
 def athlete_stats_page(athlete_id):
-    if 'user_id' not in session or not session.get('is_coach'):
-        return redirect(url_for('home_page'))
-
+    if 'user_id' not in session or not session.get('is_coach'): return redirect(url_for('home_page'))
     athlete = Archer.query.get_or_404(athlete_id)
-    if athlete.is_coach:
-        return redirect(url_for('coach_dashboard_page'))
-
-    athlete_scores = Score.query.filter_by(archer_id=athlete.id).all()
-    athlete_sight_marks = SightMark.query.filter_by(archer_id=athlete.id).all()
-    athlete_plans = TrainingPlan.query.filter_by(athlete_id=athlete.id).all()
-
-    return render_template('athlete_stats.html', athlete=athlete, scores=athlete_scores,
-                           sight_marks=athlete_sight_marks, plans=athlete_plans)
-
+    if athlete.is_coach: return redirect(url_for('coach_dashboard_page'))
+    return render_template('athlete_stats.html', athlete=athlete, scores=Score.query.filter_by(archer_id=athlete.id).all(),
+                           sight_marks=SightMark.query.filter_by(archer_id=athlete.id).all(), plans=TrainingPlan.query.filter_by(athlete_id=athlete.id).all())
 
 @app.route('/assign_plan/<int:athlete_id>', methods=['POST'])
 def assign_plan(athlete_id):
-    if 'user_id' not in session or not session.get('is_coach'):
-        return redirect(url_for('home_page'))
-
-    form_title = request.form.get('title')
-    form_desc = request.form.get('description')
-    form_date = request.form.get('date_assigned')
-
-    new_plan = TrainingPlan(title=form_title, description=form_desc, date_assigned=form_date, athlete_id=athlete_id)
-    db.session.add(new_plan)
+    if 'user_id' not in session or not session.get('is_coach'): return redirect(url_for('home_page'))
+    db.session.add(TrainingPlan(title=request.form.get('title'), description=request.form.get('description'),
+                                date_assigned=request.form.get('date_assigned'), athlete_id=athlete_id))
     db.session.commit()
     return redirect(url_for('athlete_stats_page', athlete_id=athlete_id))
-
 
 @app.route('/complete_plan/<int:plan_id>')
 def complete_plan(plan_id):
@@ -149,69 +129,71 @@ def complete_plan(plan_id):
         db.session.commit()
     return redirect(url_for('dashboard_page'))
 
-
 @app.route('/add_score', methods=['GET', 'POST'])
 def add_score_page():
     if 'user_id' not in session: return redirect(url_for('login_page'))
     if request.method == 'POST':
-        form_date = request.form.get('date')
-        form_distance = request.form.get('distance')
-        form_arrows = request.form.get('arrows_shot')
-        form_score = request.form.get('total_score')
+        # Fotograf yukleme islemi burada devreye giriyor
         form_image = request.files.get('target_image')
         filename = None
-
         if form_image and form_image.filename != '':
             filename = secure_filename(form_image.filename)
             os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
             form_image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-        new_score = Score(date=form_date, distance=form_distance, arrows_shot=form_arrows,
-                          total_score=form_score, image_file=filename, archer_id=session['user_id'])
+        new_score = Score(
+            date=request.form.get('date'),
+            distance=request.form.get('distance'),
+            arrows_shot=request.form.get('arrows_shot'),
+            total_score=request.form.get('total_score'),
+            arrow_data=request.form.get('arrow_data'),
+            arrows_per_end=request.form.get('arrows_per_end'),
+            image_file=filename,  # Resim dosyasi veritabanina kaydediliyor
+            archer_id=session['user_id']
+        )
         db.session.add(new_score)
         db.session.commit()
-        return redirect(url_for('dashboard_page'))
+        return redirect(url_for('my_trainings_page'))
     return render_template('add_score.html')
 
-
-# GUNCELENEN KISIM: Nisangah ayarina fotograf yukleme logic'i eklendi
 @app.route('/add_sight_mark', methods=['GET', 'POST'])
 def add_sight_mark_page():
     if 'user_id' not in session: return redirect(url_for('login_page'))
     if request.method == 'POST':
-        form_date = request.form.get('date')
-        form_distance = request.form.get('distance')
-        form_setting = request.form.get('setting')
-
-        # Nisangah resmi yakalama
         form_image = request.files.get('sight_image')
         filename = None
-
         if form_image and form_image.filename != '':
             filename = secure_filename(form_image.filename)
             os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
             form_image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-        new_mark = SightMark(date_recorded=form_date, distance=form_distance, setting=form_setting,
-                             image_file=filename, archer_id=session['user_id'])
-        db.session.add(new_mark)
+        db.session.add(SightMark(date_recorded=request.form.get('date'), distance=request.form.get('distance'),
+                                 setting=request.form.get('setting'), image_file=filename, archer_id=session['user_id']))
         db.session.commit()
         return redirect(url_for('dashboard_page'))
     return render_template('add_sight_mark.html')
 
+@app.route('/my_trainings')
+def my_trainings_page():
+    if 'user_id' not in session: return redirect(url_for('login_page'))
+    scores = Score.query.filter_by(archer_id=session['user_id']).order_by(Score.id.desc()).all()
+    return render_template('my_trainings.html', scores=scores)
+
+@app.route('/view_training/<int:score_id>')
+def view_training_page(score_id):
+    if 'user_id' not in session: return redirect(url_for('login_page'))
+    score = Score.query.get_or_404(score_id)
+    if score.archer_id == session['user_id'] or session.get('is_coach'):
+        return render_template('view_training.html', score=score)
+    else:
+        return redirect(url_for('home_page'))
 
 @app.route('/dashboard')
 def dashboard_page():
     if session.get('is_coach'): return redirect(url_for('coach_dashboard_page'))
     if 'user_id' in session:
-        user_scores = Score.query.filter_by(archer_id=session['user_id']).all()
-        user_sight_marks = SightMark.query.filter_by(archer_id=session['user_id']).all()
-        user_plans = TrainingPlan.query.filter_by(athlete_id=session['user_id']).all()
-        return render_template('dashboard.html', username=session['username'], scores=user_scores,
-                               sight_marks=user_sight_marks, plans=user_plans)
-    else:
-        return redirect(url_for('login_page'))
-
+        return render_template('dashboard.html', username=session['username'], scores=Score.query.filter_by(archer_id=session['user_id']).all(),
+                               sight_marks=SightMark.query.filter_by(archer_id=session['user_id']).all(), plans=TrainingPlan.query.filter_by(athlete_id=session['user_id']).all())
+    return redirect(url_for('login_page'))
 
 @app.route('/logout')
 def logout_page():
@@ -219,7 +201,6 @@ def logout_page():
     session.pop('username', None)
     session.pop('is_coach', None)
     return redirect(url_for('home_page'))
-
 
 if __name__ == '__main__':
     app.run(debug=True)
